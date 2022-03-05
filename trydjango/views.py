c9df8063d0e9254fc14a8d67c400dd4258e913a8
Roman <khabirov_rr@bk.ru>
2022-03-05 22:37:16 +0300

    lesson_16

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,4 +1,19 @@
+import random
+
 from django.http import HttpResponse
+from articles.models import Article
+
 
 def home_view(request):
-    return HttpResponse("<b>Hello</b>")
+    articles_quantity = Article.objects.count()
+    random_article_id = random.randint(1, articles_quantity)
+    article = Article.objects.get(id=random_article_id)
+
+    H1_STRING = f"""
+    <h1>{article.title} ({article.id})</h1>
+    """
+    P_STRING = f"""
+    <p>{article.content}</p>
+    """
+    HTML_STRING = H1_STRING + P_STRING
+    return HttpResponse(HTML_STRING)
