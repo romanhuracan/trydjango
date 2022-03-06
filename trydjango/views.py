a0e448507becd8f470f754c26e510bceab85de67
Roman <khabirov_rr@bk.ru>
2022-03-06 23:16:25 +0300

    lesson_18

--- trydjango/views.py
+++ trydjango/views.py
@@ -7,9 +7,10 @@ from articles.models import Article
 def home_view(request):
     random_article_id = random.randint(1, Article.objects.count())
     article = Article.objects.get(id=random_article_id)
+    object_list = Article.objects.all()
 
     return render(
         request,
         template_name='home_view.html',
-        context={'article': article}
+        context={'article': article, 'object_list': object_list}
         )
