dc4a6fb41f17df276e9a411696d21a3beb783a9c
Roman <khabirov_rr@bk.ru>
2022-03-18 19:41:59 +0300

    lesson_22

--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,18 @@ from django.shortcuts import render
 from articles.models import Article
 
 
+def article_search_view(request):
+    article = None
+    try:
+        query = int(request.GET.get('q'))
+    except:
+        query = None
+    if query is not None:
+        article = Article.objects.get(id=query)
+    context = {'object': article}
+    return render(request, 'articles/search.html', context=context)
+
+
 def article_detail_view(request, id=None):
     article = None
     if id is not None:
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import home_view
-from articles.views import article_detail_view
+from articles.views import article_detail_view, article_search_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('articles/', article_search_view),
     path('articles/<int:id>/', article_detail_view),
 ]
