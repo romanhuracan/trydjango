80d13daed41df51463fabe54c689d7fd7aa99032
Roman <khabirov_rr@bk.ru>
2022-03-19 13:56:37 +0300

    lesson_31

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c1ercv&pl*5q&#0g!g294pk0zbg@9$m=h$i&#d1s$^ricsu_))'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == '1'
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOSTS')]
 
 
 # Application definition
