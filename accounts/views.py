9f1e3697776ea7f8ade6a392961394861a2428e0
Roman <khabirov_rr@bk.ru>
2022-03-19 13:17:25 +0300

    lesson_30

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def register_view(request):
@@ -14,14 +14,14 @@ def register_view(request):
 
 def login_view(request):
     if request.method == 'POST':
-        username, password = map(request.POST.get, ['username', 'password'])
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {'error': 'Invalid username or password'}
-            return render(request, 'accounts/login.html', context)
-        login(request, user)
-        return redirect('/')
-    return render(request, 'accounts/login.html', {})
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('/')
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'accounts/login.html', context)
 
 
 def logout_view(request):
